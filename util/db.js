7c542098e6d0955e57c26d48c3e20266a5a93bd2
Ochuko George <georgeochuko7@gmail.com>
2023-11-29 12:49:03 +0100

    updated mysql connection params

--- util/db.js
+++ util/db.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
     // host: "127.0.0.1" ||'localhost' || process.env.HOST,
-    host: process.env.HOST,
+    host: process.env.DBHOST || "localhost",
     user: process.env.USER,
     port: process.env.POST, 
     database: process.env.DATABASE,
