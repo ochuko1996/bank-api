d560ca036cd5583f866789fbe7253c189cda2423
Ochuko George <georgeochuko7@gmail.com>
2023-11-29 11:51:34 +0100

    updated db with online parameters

--- util/db.js
+++ util/db.js
@@ -1,7 +1,8 @@
 import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-    host: "127.0.0.1" ||'localhost',
+    // host: "127.0.0.1" ||'localhost' || process.env.HOST,
+    host: process.env.HOST,
     user: process.env.USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
